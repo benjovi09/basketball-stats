1119a596a9170dc0bdd68e8ae41360e2fbf22000
Ben <ishouldreadmore@gmail.com>
2020-07-06 18:11:33 -0500

    <3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('Mike');
+  const [players, setPlayers] = useState([]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setPlayers(await getPlayers(name));
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  async function getPlayers(name) {
+    const url = `https://www.balldontlie.io/api/v1/players?search=${name}`;
+    return await fetch(url).then(handleFetchResponse).then(handlePlayerSearchResponse);
+  }
+
+  async function handleFetchResponse(r) {
+    if (r.ok) {
+      return await r.json();
+    } else {
+      console.log(await r.json());
+    }
+  }
+
+  async function handlePlayerSearchResponse(response) {
+    return response.data;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={name} onChange={handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>team</th>
+            </tr>
+          </thead>
+          <tbody>
+            {players.map((player) => (
+              <tr>
+                <td>{player.id}</td>
+                <td>{`${player.first_name} ${player.last_name}`}</td>
+                <td>{player.team.abbreviation}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </body>
   );
 }
 
